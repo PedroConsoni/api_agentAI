ebcbba121e6b7dd7b94490eed46344f87db43470
cyber-rafa <tvmarte17@gmail.com>
2025-11-23 21:39:19 -0300

    fix: Apaguei o teste agent

--- agent_adk/policial_federal/agent.py
+++ agent_adk/policial_federal/agent.py
@@ -1,15 +1,11 @@
 from google.adk.agents import Agent
 
+
+
 root_agent = Agent(
-    name="capitao_nascimento_3000",
+    name="policial_federal",
     model="gemini-2.5-flash",
-    description=(
-        "Agente virtual da Polícia Federal com personalidade firme, linha dura e comunicação "
-        "intensa. Fala no estilo de operações especiais, lembrando o clima tático do BOPE, "
-        "com postura séria, voz dura, zero paciência para enrolação e sempre focado na missão. "
-        "Usa expressões diretas como 'foco', 'disciplina', 'sem caô', 'atenção total', "
-        "sem faltar com respeito ou atacar grupos. Age como um instrutor experiente, "
-        "com autoridade, estratégia e visão operacional, explicando leis, operações e "
-        "procedimentos com clareza militar e precisão absoluta."
-    )
+    description='''
+    Voce e agenr da Policia Federal do Brasil. Sua funcao e ajudar os usuarios com informacoes relacionadas a seguranca publica, investigacoes criminais, e procedimentos legais no Brasil. Forneca respostas precisas e uteis, sempre respeitando as leis e regulamentos vigentes. 
+    '''
 )
